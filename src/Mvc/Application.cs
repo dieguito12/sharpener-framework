04b1398c7fecf4139bca0184388fd86e4440e687
Diego Mena <Diego Mena>
2017-02-24 17:33:10 -0400

    Starts web server

---
+++ src/Mvc/Application.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PHttp.Application;
+
+namespace Mvc
+{
+    public class Application : IPHttpApplication
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public event PreApplicationStartMethod PreApplicationStart;
+        public event ApplicationStartMethod ApplicationStart;
+
+        public void Start()
+        {
+            Console.WriteLine("Start Method");
+        }
+
+        public void ExecuteAction()
+        {
+            Console.WriteLine("ExecuteAction Method");
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("This is a Test Method");
+        }
+    }
+}
---
+++ src/PHttp/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace PHttp
+{
+    public class Startup
+    {
+        public static List<Application.IPHttpApplication> LoadApps(string path)
+        {
+            List<Application.IPHttpApplication> apps = new List<Application.IPHttpApplication>();
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileInfo[] fis = di.GetFiles("*.dll");
+            foreach (FileInfo fls in fis)
+            {
+                var assembly = Assembly.LoadFrom(fls.FullName);
+                var myObjects = (from t in assembly.GetTypes()
+                                 where t.GetInterfaces().Contains(typeof(Application.IPHttpApplication))
+                                 select (Application.IPHttpApplication)Activator.CreateInstance(t)).ToList();
+
+                apps = apps.Concat(myObjects).ToList();
+            }
+
+            foreach(var app in apps)
+            {
+                app.Start();
+            }
+            return apps;
+        }
+    }
+}
